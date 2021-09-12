cbae7912b7b2c3f02311673d7ce80e7d5be1a8bd
purple-thistle <sashan@vaiolabs.com>
2021-09-12 05:48:09 +0300

    Structure clean-up

---
+++ deployment/deployment.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash 
+
+##########################################
+# author: purple-thistle
+# goal: to deploy the flask app 
+# 
+##########################################
+
+
+#check for sudo priviliges
+check_privilliges() {
+A=$(sudo -n -v 2>&1)
+res=$(echo "$A" | grep -q password )
+if [[ -z $A || -z $res ]]; then
+    echo "can use sudo"
+    return 0
+else
+    return 1    
+fi    
+}
+
+
+#set package installer name
+YUM_CMD=$(which yum)
+APT_CMD=$(which apt)
+
+
+if [[ -n $YUM_CMD ]]; then
+    PACK_INST="yum"
+elif [[ -n $APT_CMD ]]; then
+    PACK_INST="apt"
+else
+    echo "package installer not defined"
+    exit 1    
+fi
+
+
+#check if pip is installed 
+PIP_CMD="$(which pip)"
+if [[ -z $PIP_CMD ]]; then
+    if [[ $(check_privilliges) ]]; then
+        echo "installing pip"
+        sudo $PACK_INST install python3-pip
+    else
+        echo "pip is not installed and you do not have priviliges to install it"
+        exit 1
+    fi    
+fi
+
+#check if postgresql is installed
+PSQL_CMD=$(which psql)
+if [[ -z $PSQL_CMD ]]; then
+    if [[ $(check_privilliges) ]]; then
+        echo "installing postgresql"
+        sudo $PACK_INST install postgresql
+    else
+        echo "postgresql is not installed and you do not have priviliges to install it"
+        exit 1
+    fi    
+fi
+
+    
+#create database and table
+createdb -h localhost -U postgres learningflask
+
+psql -h localhost -U postgres learningflask -c "CREATE TABLE users (uid serial PRIMARY KEY,
+        firstname VARCHAR(100) not null,
+        lastname VARCHAR(1000) not null,
+        email VARCHAR(120) not null unique,
+        pwdhash VARCHAR(100) not null);"
+
+
+#install virtual environment    
+pip3 install virtualenv
+
+
+#create and connect to virtual environment
+virtualenv venv
+source venv/bin/activate
+
+
+#install requirements
+pip3 install -r requirements.txt
+
+
+python routes.py
