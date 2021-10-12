5d3448cdb90827409e9055b51e510e516833b535
Purple Thistle <sashan@vaiolabs.com>
2021-10-12 14:43:36 +0300

    nginx conf

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -59,6 +59,18 @@ if [[ -z $PSQL_CMD ]]; then
     fi    
 fi
 
+
+#check if nginx is installed
+PSQL_CMD=$(which nginx)
+if [[ -z $PSQL_CMD ]]; then
+    if [[ $(check_privilliges) ]]; then
+        echo "installing nginx"
+        sudo $PACK_INST install nginx
+    else
+        echo "nginx is not installed and you do not have priviliges to install it"
+        exit 1
+    fi    
+fi
     
 #create database and table
 echo "Creating database."
@@ -79,4 +91,26 @@ pip3 install pipenv
 #install requirements from Pipfile
 pipenv install
 
-pipenv run python ../application/routes.py
+
+#configure nginx
+systemctl start nginx
+
+#for Ubuntu/Debian
+if [[ "$PACK_INST" == "apt" ]]; then
+    
+    cp flask_app.conf /etc/nginx/sites-available
+
+    ln -s /etc/nginx/sites-available/flask_app.conf etc/nginx/sites-enabled/flask_app.conf    
+
+#for Centos/RedHat/Fedora
+elif [[ "$PACK_INST" == "yum" ]]; then
+
+    cp flask_app.conf /etc/nginx/conf.d
+   
+fi
+
+systemctl restart nginx
+
+#start the app with gunicorn
+echo "go to 'localhost' to view the app"
+gunicorn -w4 --bind 0.0.0.0:5000 wsgi:app
